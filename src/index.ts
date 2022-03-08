4de8b64b2603471f86c1a3f6753345463a252fc1
JianpengHe <i@hejianpeng.cn>
2022-03-09 01:29:09 +0800

    feat: UnZip

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,38 @@
-import { XML } from "./node/XML";
-import { Buf } from "./node/Buf";
-import { Mysql } from "./node/mysql";
-const text = `<xml>
-  <events date="01-10-2009" color="0x99CC00" selected="true"> 
-  <a y="53"></a>
-     <event>
-          <title>You can use HTML and CSS</title>
-          <description><![CDATA[This is the description ]]></description>
-          <description1>![CDATA[This is the description ]]</description1>
-      </event>
-  </events>
-  <b><txt>566
-  677
-  </txt><doc>
-  <a
-     a2="2"   a1="1"
-  >123</a>
-</doc></b>
-</xml>`;
-const time = 1e5;
-console.log(XML.parseTerse(text).xml);
-console.time("递归");
-for (let index = 0; index < time; index++) {
-  XML.parseTerse(text);
-}
-console.timeEnd("递归");
+import { UnZip } from "./node/UnZip";
+import * as http from "http";
+import * as fs from "fs";
+const a = new UnZip(fs.createReadStream("tools.zip"), __dirname + "/unzip/");
+// http.get("http://xxx.com/test.zip", res => {
+//   const a = new UnZip(res, __dirname + "/unzip/");
+// });
+
+// import { XML } from "./node/XML";
+// import { Buf } from "./node/Buf";
+// import { Mysql } from "./node/mysql";
+// const text = `<xml>
+//   <events date="01-10-2009" color="0x99CC00" selected="true">
+//   <a y="53"></a>
+//      <event>
+//           <title>You can use HTML and CSS</title>
+//           <description><![CDATA[This is the description ]]></description>
+//           <description1>![CDATA[This is the description ]]</description1>
+//       </event>
+//   </events>
+//   <b><txt>566
+//   677
+//   </txt><doc>
+//   <a
+//      a2="2"   a1="1"
+//   >123</a>
+// </doc></b>
+// </xml>`;
+// const time = 1e5;
+// console.log(XML.parseTerse(text).xml);
+// console.time("递归");
+// for (let index = 0; index < time; index++) {
+//   XML.parseTerse(text);
+// }
+// console.timeEnd("递归");
 
 // console.time("循环");
 // for (let index = 0; index < time; index++) {
---
+++ src/node/UnZip.ts
@@ -0,0 +1,74 @@
+import * as fs from "fs";
+import * as path from "path";
+import * as zlib from "zlib";
+import { Buf } from "./Buf";
+import { IGetLengthFn, IReadStream, RecvAll, RecvStream } from "./RecvStream";
+export class UnZip {
+  private dirCache: Set<string> = new Set();
+  private outputPath: string;
+  private getLengthFn: IGetLengthFn = (headerBuf, readBufferFn) => {
+    const buf = new Buf(headerBuf);
+    const info = {
+      head: buf.readUIntLE(4),
+      version: buf.readUIntLE(2),
+      flag: buf.readUIntLE(2),
+      compressionMethod: buf.readUIntLE(2),
+      lastModificationTime: buf.readUIntLE(2),
+      lastModificationDate: buf.readUIntLE(2),
+      CRC: buf.read(4),
+      compressedSize: buf.readUIntLE(4),
+      uncompressedSize: buf.readUIntLE(4),
+      fileNameLength: buf.readUIntLE(2),
+      extraFieldLength: buf.readUIntLE(2),
+      fileName: "",
+    };
+    if (info.head !== 0x04034b50) {
+      return;
+    }
+    const ReadStream = readBufferFn(info.compressedSize + info.fileNameLength + info.extraFieldLength);
+    let needReadFileNameSize = info.fileNameLength + info.extraFieldLength;
+    const fileNameBufs: Buffer[] = [];
+    const needReadFileName = () => {
+      ReadStream.once("readable", () => {
+        let data: Buffer;
+        while (needReadFileNameSize && (data = ReadStream.read(needReadFileNameSize))) {
+          if (data && data.length) {
+            needReadFileNameSize -= data.length;
+            fileNameBufs.push(data);
+          }
+        }
+        if (needReadFileNameSize) {
+          needReadFileName();
+        } else {
+          info.fileName = path.join(this.outputPath, String(Buffer.concat(fileNameBufs).slice(0, info.fileNameLength)));
+          const { dir } = path.parse(info.fileName);
+          if (!this.dirCache.has(dir)) {
+            fs.mkdir(dir, { recursive: true }, () => {
+              this.dirCache.add(dir);
+              writeFile();
+            });
+          } else {
+            writeFile();
+            //process.nextTick(writeFile);
+          }
+        }
+      });
+    };
+    needReadFileName();
+    const writeFile = () => {
+      if (!info.compressedSize) {
+        RecvAll(ReadStream);
+        return;
+      }
+      if (info.compressionMethod === 8 || info.compressionMethod === 9) {
+        ReadStream.pipe(zlib.createInflateRaw()).pipe(fs.createWriteStream(info.fileName));
+      } else {
+        ReadStream.pipe(fs.createWriteStream(info.fileName));
+      }
+    };
+  };
+  constructor(readStream: IReadStream, outputPath: string = __dirname + "/unzip/") {
+    this.outputPath = outputPath;
+    new RecvStream(readStream, 30, this.getLengthFn);
+  }
+}
