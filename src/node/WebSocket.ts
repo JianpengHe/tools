4cd2200aa2df1cc36511be2d1ebd566c023f0a88
何健鹏 <i@hejianpeng.cn>
2022-11-24 18:15:49 +0800

    feat: WebSocket完整版

--- src/node/WebSocket.ts
+++ src/node/WebSocket.ts
@@ -3,7 +3,8 @@ import * as http from "http";
 import * as stream from "stream";
 import * as crypto from "crypto";
 import { RecvStream } from "./RecvStream";
-//class TTT
+import { TypedEventEmitter } from "./utils";
+import { Buf } from "./Buf";
 export enum EWebSocketOpcode {
   "附加数据帧" = 0,
   "文本数据帧" = 1,
@@ -12,15 +13,25 @@ export enum EWebSocketOpcode {
   "ping" = 9,
   "pong" = 10,
 }
-export class WebSocket extends stream.Duplex {
+export type IWebSocketEvents = {
+  connected: () => void;
+  subStream: (subStream: stream.Readable) => void;
+  text: (text: string) => void;
+  error: (err: Error) => void;
+  close: () => void;
+};
+
+export class WebSocket extends TypedEventEmitter<IWebSocketEvents> {
   public isWebSocket = false;
   private socket: net.Socket;
   private recvStream?: RecvStream;
-  public opts: { onText?: (text: string) => void; maxTextSize?: number };
+  public opts: { maxTextSize?: number; recvHighWaterMark?: number; sendHighWaterMark?: number };
   constructor(req: http.IncomingMessage, res: http.ServerResponse, opts?: WebSocket["opts"]) {
     super();
     this.opts = opts || {};
     this.opts.maxTextSize = this.opts.maxTextSize ?? 64 * 1024 * 1024;
+    this.opts.recvHighWaterMark = this.opts.recvHighWaterMark ?? 65536;
+    this.opts.sendHighWaterMark = this.opts.sendHighWaterMark ?? 65536;
     this.socket = req.socket;
     if (req.headers.connection === "Upgrade" && req.headers["sec-websocket-key"]) {
       res.writeHead(101, {
@@ -35,168 +46,234 @@ export class WebSocket extends stream.Duplex {
       this.recvStream = new RecvStream(this.socket);
       this.socket.resume();
       this.isWebSocket = true;
-      this.canPushLen = 4;
       process.nextTick(() => {
+        this.emit("connected");
         this.recvData();
       });
-      this.socket.once("close", () => {
-        this.push(null);
-      });
+      this.socket.once("close", () => this.emit("close"));
     } else {
-      this.push(null);
+      this.emit("close");
     }
   }
-  /** 可以接受的长度 */
-  private canPushLen?: number;
-  /** 已解密的临时buffer */
-  private recvBuffer: Buffer[] = [];
-  private recvBufferLen = 0;
-  /** 当前ws包的剩余大小 */
-  private recvLen = 0;
-  /** 当前ws包的类型 */
-  private recvType = 0;
-  /** 当前ws包是否已结束 */
-  private recvIsEnd = true;
-  /** 当前ws包的掩码 */
-  private recvMask: number[] = [];
-  /** 当前ws包掩码的序号 */
-  private recvMaskIndex: number = 0;
-  /** 接收函数上锁 */
-  private recvlock = false;
   /** 接收浏览器传过来的数据 */
   private async recvData() {
-    if (this.recvlock) {
-      return;
-    }
-    this.recvlock = true;
-    while (this.canPushLen) {
-      if (!this.recvStream) {
-        throw new Error("no recvStream");
-      }
-      if (this.recvLen === 0) {
-        this.recvMaskIndex = 0;
-        let needRead = 2;
-        let readLen = false;
-        let readMask = false;
-        while (needRead) {
-          const bufPo = this.recvStream.readBufferSync(needRead);
-          const buffer = bufPo instanceof Promise ? await bufPo : bufPo;
-          // console.log(buffer);
-          if (readLen === false && readMask === false) {
-            const flag = buffer[0];
-            this.recvIsEnd = flag >= 128;
-            this.recvType = flag % 128 || this.recvType;
-            /** 如果接收到未知的opcode，接收端必须关闭连接 */
-            if (EWebSocketOpcode[this.recvType] === undefined) {
-              this.socket.end();
-              this.emit("error", new Error("接收到未知的opcode"));
-              return;
-            }
-            const len = buffer[1];
-            if (len >= 128) {
-              readMask = true;
-            }
-            switch (len % 128) {
-              case 127:
-                readLen = true;
-                needRead = 8;
-                break;
-              case 126:
-                readLen = true;
-                needRead = 2;
-                break;
-              default:
-                this.recvLen = len % 128;
-                readLen = false;
-                needRead = readMask ? 4 : 0;
-            }
-          } else if (readLen === true) {
-            this.recvLen = [...buffer].reduce((a, b) => a * 256 + b);
-            needRead = readMask ? 4 : 0;
-            readLen = false;
-          } else if (readMask === true) {
-            this.recvMask = [...buffer];
-            readMask = false;
-            needRead = 0;
+    /** 接收一个完整的ws包 */
+    while (1) {
+      let recvType: number = 0;
+      let subReadable: stream.Readable | undefined;
+      let canReadSize = Number(this.opts?.recvHighWaterMark);
+      let canReadFn = (value: boolean) => {};
+      let canReadPush: ((value: Buffer | null) => void) | undefined;
+      const buffers: Buffer[] = [];
+      let buffersLen: number = 0;
+      while (1) {
+        /** 接收一个ws分包 */
+        if (!this.recvStream) {
+          const err = new Error("no recvStream");
+          if (!this.emit("error", err)) {
+            throw err;
+          }
+          return;
+        }
+
+        /** 接收前2个字节 */
+        const headBufPo = this.recvStream.readBufferSync(2);
+        const headBuf = headBufPo instanceof Promise ? await headBufPo : headBufPo;
+        const flag = headBuf[0];
+        const recvIsEnd = flag >= 128;
+        let needReadMask = false;
+        let recvMask: number[] = [0, 0, 0, 0];
+        let recvLen = 0;
+        recvType = flag % 128 || recvType;
+        if (!subReadable && recvType === 2) {
+          subReadable = new stream.Readable({
+            read(size) {
+              if (!canReadPush) {
+                canReadPush = this.push.bind(this);
+              }
+              // console.log("可以收");
+              canReadSize = size;
+              canReadFn(true);
+              canReadFn = () => {};
+            },
+          });
+          if (!this.emit("subStream", subReadable)) {
+            console.warn("未添加subStream事件监听器，websocket的所有stream都会被丢弃");
+            subReadable = undefined;
           }
         }
-        if (this.recvType === EWebSocketOpcode["连接关闭"]) {
+
+        /** 如果接收到未知的opcode，接收端必须关闭连接 */
+        if (EWebSocketOpcode[recvType] === undefined) {
+          this.socket.end();
+          const err = new Error("接收到未知的opcode");
+          if (!this.emit("error", err)) {
+            throw err;
+          }
+          return;
+        }
+        if (recvType === EWebSocketOpcode["连接关闭"]) {
           this.socket.end();
           return;
         }
-        // console.log("recvLen", this.recvLen, "recvType", this.recvType);
-      }
-      if (this.recvLen) {
-        const bufPo = this.recvStream.readBufferSync(this.recvLen);
-        const buf = bufPo instanceof Promise ? await bufPo : bufPo;
-        /** 根据mask解密 */
-        buf.forEach((a, i) => {
-          buf[i] ^= this.recvMask[this.recvMaskIndex++];
-          this.recvMaskIndex %= 4;
-        });
-        this.recvLen -= buf.length;
-        this.recvBuffer.push(buf);
-        this.recvBufferLen += buf.length;
-        if (this.recvType === EWebSocketOpcode["文本数据帧"] && this.opts.onText && !this.recvIsEnd) {
-          if (this.recvBufferLen > Number(this.opts.maxTextSize)) {
-            this.emit(
-              "error",
-              new Error(
-                "text too loog,maxTextSize" + Number(this.opts.maxTextSize) + ",nowRecvSize:" + this.recvBufferLen
-              )
-            );
-            this.socket.end();
-            return;
-          }
-          continue;
-        }
-        if (this.recvBuffer.length > 1) {
-          this.recvBuffer[0] = Buffer.concat(this.recvBuffer);
-          this.recvBuffer.length = 1;
-        }
-        const buffer = this.recvBuffer[0];
-        this.recvBufferLen = 0;
-        this.recvBuffer.length = 0;
 
-        if (this.recvType === EWebSocketOpcode["文本数据帧"] && this.opts.onText) {
-          this.opts.onText(String(buffer));
-          this.canPushLen = 4;
-        } else {
-          this.push(buffer);
-          this.canPushLen -= buffer.length;
+        recvLen = headBuf[1];
+        if (recvLen >= 128) {
+          needReadMask = true;
+        }
+        recvLen %= 128;
+        if (recvLen > 125) {
+          const lenBufPo = this.recvStream.readBufferSync(recvLen === 126 ? 2 : 8);
+          recvLen = [...(lenBufPo instanceof Promise ? await lenBufPo : lenBufPo)].reduce((a, b) => a * 256 + b);
+        }
+
+        if (needReadMask) {
+          const maskBufPo = this.recvStream.readBufferSync(4);
+          recvMask = [...(maskBufPo instanceof Promise ? await maskBufPo : maskBufPo)];
+        }
+        // console.log("recvLen", recvLen, "recvType", recvType);
+        if (!recvLen) {
+          break;
+        }
+        let recvMaskIndex = 0;
+        /** 再根据recvHighWaterMark大小分包 */
+        while (recvLen > 0) {
+          const nowReadSize = Math.min(canReadSize, recvLen, Number(this.opts?.recvHighWaterMark));
+
+          if (nowReadSize <= 0 || (subReadable && !canReadPush)) {
+            /** 等一等 */
+            await new Promise(r => {
+              canReadFn = r;
+            });
+            continue;
+          }
+          const bufPo = this.recvStream.readBufferSync(nowReadSize);
+          const buf = bufPo instanceof Promise ? await bufPo : bufPo;
+          buf.forEach((_, i) => {
+            buf[i] ^= recvMask[recvMaskIndex++];
+            recvMaskIndex %= 4;
+          });
+          recvLen -= buf.length;
+          /** TODO */
+
+          if (subReadable && canReadPush) {
+            // console.log("发送", buf.length);
+            canReadPush(buf);
+            canReadSize = 0;
+          } else if (recvType === 1) {
+            buffersLen += buf.length;
+            buffers.push(buf);
+            if (buffersLen > Number(this.opts.maxTextSize)) {
+              const err = new Error(
+                "text too loog,maxTextSize" + Number(this.opts.maxTextSize) + ",nowRecvSize:" + buffersLen
+              );
+              if (this.emit("error", err)) {
+                throw err;
+              }
+              this.emit("close");
+              this.socket.end();
+              return;
+            }
+          }
+        }
+        if (recvIsEnd) {
           break;
         }
       }
+      if (recvType === 2 && canReadPush) {
+        canReadPush(null);
+        canReadSize = 0;
+      } else if (recvType === 1) {
+        this.emit("text", String(Buffer.concat(buffers)));
+      }
+
+      // console.log("recvLen", this.recvLen, "recvType", this.recvType);
     }
-    this.recvlock = false;
   }
-  /** 可以接收浏览器发过来多少数据 */
-  async _read(len: number) {
-    this.canPushLen = len;
-    this.recvData();
+  private sendQueue: (Buffer | stream.Readable | string)[] = [];
+  public sendBusy = false;
+  public send(data: Buffer | stream.Readable | string) {
+    this.sendQueue.push(data);
+    this.tryToCleanQueue();
+    return this;
   }
 
-  _write(chunk, encoding, callback) {
-    console.log(chunk);
-    return false;
+  private async tryToCleanQueue() {
+    if (this.sendBusy || this.sendQueue.length === 0) {
+      return;
+    }
+    this.sendBusy = true;
+    const task = this.sendQueue.splice(0, 1)[0];
+    let isStart = true;
+    const makeHeadBuf = (type: number, len: number) => {
+      const buf = new Buf();
+      if (isStart) {
+        buf.writeUIntBE(type, 1);
+        isStart = false;
+      } else {
+        buf.writeUIntBE(0, 1);
+      }
+      if (len < 126) {
+        buf.writeUIntBE(len, 1);
+      } else if (len < 65536) {
+        buf.writeUIntBE(126, 1).writeUIntBE(len, 2);
+      } else {
+        buf.writeUIntBE(127, 1).writeUIntBE(len, 8);
+      }
+      return buf;
+    };
+    if (task instanceof stream.Readable) {
+      task.on("data", chunk => {
+        if (!this.socket.write(Buffer.concat([makeHeadBuf(2, chunk.length).buffer, chunk]))) {
+          task.pause();
+          this.socket.once("drain", () => {
+            task.resume();
+          });
+        }
+      });
+      task.on("end", () => {
+        this.socket.write(Buffer.from([128, 0]));
+      });
+    } else {
+      const isText = !(task instanceof Object);
+      const dataBuf = new Buf(Buffer.from(task));
+      while (dataBuf.offset < dataBuf.buffer.length) {
+        const chunk = dataBuf.read(Number(this.opts.sendHighWaterMark));
+        const buf = makeHeadBuf(isText ? 1 : 2, chunk.length);
+        if (dataBuf.offset === dataBuf.buffer.length) {
+          buf.buffer[0] += 128;
+        }
+        if (!this.socket.write(Buffer.concat([buf.buffer, chunk]))) {
+          await new Promise(r => {
+            this.socket.once("drain", () => {
+              r(1);
+            });
+          });
+        }
+      }
+    }
   }
 }
 
 // 测试用例
-http
-  .createServer((req, res) => {
-    const t = new WebSocket(req, res, {
-      onText(txt) {
-        console.log("文字", txt);
-      },
-    });
-    t.on("data", d => console.log("收到", d));
-    t.on("end", () => {
-      console.log("close");
-    });
-    t.on("error", e => {
-      console.log(e);
-    });
-    t.isWebSocket || res.end("404");
-  })
-  .listen(80);
+// http
+//   .createServer((req, res) => {
+//     const t = new WebSocket(req, res, {})
+//       .on("subStream", subStream => {
+//         subStream.on("data", c => console.log("收到", c));
+//         subStream.on("end", () => console.log("结束"));
+//         // subStream.pipe(require("fs").createWriteStream("ttt.bin"));
+//       })
+//       .on("text", txt => {
+//         console.log("文字", txt);
+//       })
+//       .on("connected", () => {
+//         t.send(require("fs").createReadStream("ttt.bin"));
+//         //t.send("1234567890".repeat(1024 * 10));
+//       })
+//       .on("error", e => {
+//         console.log(e);
+//       });
+//     t.isWebSocket || res.end("404");
+//   })
+//   .listen(80);
