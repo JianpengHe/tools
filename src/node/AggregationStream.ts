ace7abb5e5c7661006515e170ab8292a4649becb
JianpengHe <i@hejianpeng.cn>
2023-04-26 16:52:35 +0800

    test: 添加测试用例：分块下载

--- src/node/AggregationStream.ts
+++ src/node/AggregationStream.ts
@@ -73,3 +73,52 @@ export class AggregationStream extends stream.Readable {
 //         })
 //     ).pipe(fs.createWriteStream("D:/test2.bin"));
 //   });
+
+// 测试用例（分片下载）
+// import * as https from "https";
+// import * as fs from "fs";
+// /** 显示网速，非必须 */
+// import { ShowTransferProgress } from "./ShowTransferProgress";
+
+// const downloadUrl =
+//   "https://f.c2r.ts.cdn.office.net/pr/55336b82-a18d-4dd6-b5f6-9e5095c314a6/Office/Data/16.0.16130.20394/stream.x64.x-none.dat";
+
+// const maxSockets = 10;
+// /** 每次下载的大小 */
+// const bufferSizePerRes = 1024 * 1024;
+// const agent = new https.Agent({ maxSockets });
+// https.get(downloadUrl, { agent }, res => {
+//   const size = Number(res.headers["content-length"] || 0);
+//   if (!res.headers["accept-ranges"] || !size) {
+//     console.log(res.headers);
+//     throw new Error("服务端不允许分片下载或文件大小为0");
+//   }
+//   const fileName = (String(res.headers["content-disposition"] || "").match(/filename=([^;]+)/) || [])[1] || "data.bin";
+//   /** 显示网速，非必须 */
+//   const showTransferProgress = new ShowTransferProgress(fileName, size);
+//   console.log("启动服务，共", Math.ceil(size / bufferSizePerRes), "分片");
+//   new AggregationStream(
+//     Math.ceil(size / bufferSizePerRes),
+//     maxSockets,
+//     index =>
+//       new Promise(r =>
+//         https.get(
+//           downloadUrl,
+//           {
+//             agent,
+//             headers: {
+//               range: `bytes=${bufferSizePerRes * index}-${Math.min(size, bufferSizePerRes * (index + 1) - 1)}`,
+//             },
+//           },
+//           async splitRes => {
+//             const body: Buffer[] = [];
+//             for await (const chuck of splitRes) {
+//               body.push(chuck);
+//               showTransferProgress.add(chuck.length);
+//             }
+//             r(Buffer.concat(body));
+//           }
+//         )
+//       )
+//   ).pipe(fs.createWriteStream(fileName));
+// });
