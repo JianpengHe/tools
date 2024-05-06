717da80f8f322cf64e6059b3f4842de537104ba0
何健鹏 <i@hejianpeng.cn>
2024-05-06 17:06:37 +0800

    test: 增加测试用例

--- src/node/TcpProxy.ts
+++ src/node/TcpProxy.ts
@@ -214,3 +214,33 @@ export class TcpProxy {
 //     });
 //   },
 // });
+
+// 使用例子2
+// import { socks5 } from "./systemNetworkSettings";
+
+// const port = 443;
+// const tcpProxy = new TcpProxy(new DnsServer());
+// tcpProxy.add({
+//   host: "pub.dev",
+//   port,
+//   connectionListener: sock =>
+//     socks5({ host: "pub.dev", port }).then(sock2 => {
+//       console.log("代理", "pub.dev", port);
+//       sock.pipe(sock2);
+//       sock2.pipe(sock);
+//       sock.on("error", e => console.log(e));
+//       sock2.on("error", e => console.log(e));
+//     }),
+// });
+// tcpProxy.add({
+//   host: "maven.google.com",
+//   port,
+//   connectionListener: sock =>
+//     socks5({ host: "maven.google.com", port }).then(sock2 => {
+//       console.log("代理", "maven.google.com", port);
+//       sock.pipe(sock2);
+//       sock2.pipe(sock);
+//       sock.on("error", e => console.log(e));
+//       sock2.on("error", e => console.log(e));
+//     }),
+// });
