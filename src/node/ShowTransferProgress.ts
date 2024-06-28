6e33813f9fa90a80e5249e295a06feeb9153831e
何健鹏 <i@hejianpeng.cn>
2024-06-28 13:40:21 +0800

    feat: 提供更多自定义配置

--- src/node/ShowTransferProgress.ts
+++ src/node/ShowTransferProgress.ts
@@ -1,58 +1,95 @@
 import { Console, EConsoleStyle } from "./Console";
-
+export enum EShowTransferProgressDisplay {
+  瞬间速度,
+  平均速度,
+  进度条,
+  剩余大小,
+  剩余时间,
+  文件名,
+}
+export type IShowTransferProgressOpt = {
+  title?: string; //"ShowTransferProgress",
+  totalSize?: number; // 0
+  interval?: number; // 1000
+  console?: Console; //  new Console()
+  display?: (EShowTransferProgressDisplay | (() => string))[];
+};
 export class ShowTransferProgress {
-  private startTime: number;
-  private title: string;
-  private filesize: number;
-  private totalSize: number;
-  private showedFilesize: number;
+  private startTime = new Date().getTime();
+  private filesize = 0;
+  private showedFilesize = 0;
   private showedTime: number;
   private timer: number;
-  private console: Console;
-  constructor(title = "ShowTransferProgress", totalSize = 0, interval = 1000, console = new Console()) {
-    this.startTime = new Date().getTime();
-    this.title = title;
-    this.filesize = 0;
-    this.totalSize = totalSize;
-    this.showedFilesize = 0;
+  private opt: Required<IShowTransferProgressOpt>;
+  constructor(opt: IShowTransferProgressOpt) {
+    opt.console = opt.console ?? new Console();
+    opt.display =
+      opt.display ??
+      (opt.totalSize
+        ? [
+            EShowTransferProgressDisplay.瞬间速度,
+            EShowTransferProgressDisplay.平均速度,
+            EShowTransferProgressDisplay.进度条,
+            EShowTransferProgressDisplay.剩余大小,
+            EShowTransferProgressDisplay.剩余时间,
+            EShowTransferProgressDisplay.文件名,
+          ]
+        : [
+            EShowTransferProgressDisplay.瞬间速度,
+            EShowTransferProgressDisplay.平均速度,
+            EShowTransferProgressDisplay.文件名,
+          ]);
+    this.opt = {
+      title: "ShowTransferProgress",
+      totalSize: 0,
+      interval: 0,
+      ...opt,
+    } as Required<IShowTransferProgressOpt>;
     this.showedTime = this.startTime;
-    this.timer = Number(setInterval(this.setInterval.bind(this), interval));
-    this.console = console;
+    this.timer = Number(setInterval(this.setInterval.bind(this), opt.interval ?? 1000));
   }
-  add(filesize: number) {
+  public add(filesize: number) {
     this.filesize += filesize;
-    if (this.totalSize === this.filesize) {
+    if (this.opt.totalSize === this.filesize) {
       this.end();
     }
   }
-  setInterval() {
+  private setInterval() {
     const now = new Date().getTime();
     const speed = ((this.filesize - this.showedFilesize) * 1000) / (now - this.showedTime);
-    const time = Math.ceil((this.totalSize - this.filesize) / speed);
-    this.console.write(
-      `瞬间速度 ${this.showSize(speed)}/s` +
-        ` 平均速度 ${this.showSize((this.filesize * 1000) / (now - this.startTime))}/s` +
-        (this.totalSize
-          ? ` ${Console.getProgressBar(this.filesize / this.totalSize)} ${(
-              (this.filesize * 100) /
-              this.totalSize
-            ).toFixed(2)}%` +
-            ` 剩余大小 ${this.showSize(this.totalSize - this.filesize)}` +
-            ` 剩余时间 ${speed ? String((time / 60) | 0).padStart(2, "0") : "--"}:${
-              speed ? String(time % 60).padStart(2, "0") : "--"
-            }`
-          : "") +
-        Console.setStringColor(" " + this.title, EConsoleStyle.blue)
+    const time = Math.ceil((this.opt.totalSize - this.filesize) / speed);
+    const showTransferProgressDisplay: { [x in EShowTransferProgressDisplay]: () => string } = {
+      [EShowTransferProgressDisplay.瞬间速度]: () => `瞬间速度 ${ShowTransferProgress.showSize(speed)}/s`,
+      [EShowTransferProgressDisplay.平均速度]: () =>
+        `平均速度 ${ShowTransferProgress.showSize((this.filesize * 1000) / (now - this.startTime))}/s`,
+      [EShowTransferProgressDisplay.进度条]: () =>
+        `${Console.getProgressBar(this.filesize / this.opt.totalSize)} ${(
+          (this.filesize * 100) /
+          this.opt.totalSize
+        ).toFixed(2)}%`,
+      [EShowTransferProgressDisplay.剩余大小]: () =>
+        `剩余大小 ${ShowTransferProgress.showSize(this.opt.totalSize - this.filesize)}`,
+      [EShowTransferProgressDisplay.剩余时间]: () =>
+        `剩余时间 ${speed ? String((time / 60) | 0).padStart(2, "0") : "--"}:${
+          speed ? String(time % 60).padStart(2, "0") : "--"
+        }`,
+      [EShowTransferProgressDisplay.文件名]: () => Console.setStringColor(" " + this.opt.title, EConsoleStyle.blue),
+    };
+    // const writeText=
+    this.opt.console.write(
+      this.opt.display
+        .map(item => (typeof item === "function" ? item() : showTransferProgressDisplay[item]()))
+        .join(" ")
     );
     this.showedFilesize = this.filesize;
     this.showedTime = now;
   }
-  end() {
+  public end() {
     clearInterval(this.timer);
     this.setInterval();
-    this.console.reset();
+    this.opt.console.reset();
   }
-  showSize(byte: number) {
+  static showSize(byte: number) {
     return "kMGTP"
       .split("")
       .reduce((a, b) => (Number(a[0]) > 1024 ? [Number(a[0]) / 1024, b + "iB"] : a), [byte, "B"])
